ebace1087ab47711ef7fa389ef1447a3e9b33fff
moulika-siva <sivalenka.m@northeastern.edu>
2025-04-13 18:10:06 -0400

    changing the system update page with buttons

--- app/src/pages/21_System_Update.py
+++ app/src/pages/21_System_Update.py
@@ -8,21 +8,19 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('App Administration Page')
+st.title('System Update Page')
 
 st.write('\n\n')
 st.write('## Model 1 Maintenance')
 
-st.button("Train Model 01", 
+st.button("Add ID", 
             type = 'primary', 
             use_container_width=True)
 
-st.button('Test Model 01', 
+st.button('Add Datatype', 
             type = 'primary', 
             use_container_width=True)
 
-if st.button('Model 1 - get predicted value for 10, 25', 
-             type = 'primary',
-             use_container_width=True):
-  results = requests.get('http://api:4000/c/prediction/10/25').json()
-  st.dataframe(results)
+st.button('Add Release Date', 
+            type = 'primary', 
+            use_container_width=True)
