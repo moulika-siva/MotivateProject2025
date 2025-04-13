c30ff35dc595b7e4b28c683658498c1838bd0213
moulika-siva <sivalenka.m@northeastern.edu>
2025-04-13 18:40:27 -0400

    updates

--- app/src/pages/20_Admin_Home.py
+++ app/src/pages/20_Admin_Home.py
@@ -11,7 +11,7 @@ SideBarLinks()
 
 st.title('System Admin Home Page')
 
-if st.button('Update ML Models', 
+if st.button('Update Any System Updates', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/21_ML_Model_Mgmt.py')
+  st.switch_page('pages/21_System_Update.py')
