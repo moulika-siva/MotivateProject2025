8b6b54637a144b691c107f1dc5343f0f6a9a6565
Era Shah <110404989+erashah@users.noreply.github.com>
2025-04-13 22:55:45 -0400

    Welcome msg for student

--- app/src/pages/05_Student_Home.py
+++ app/src/pages/05_Student_Home.py
@@ -9,17 +9,7 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome Political Strategist, {st.session_state['first_name']}.")
+st.title(f"Welcome student, {st.session_state['Name']}.")
 st.write('')
 st.write('')
-st.write('### What would you like to do today?')
-
-if st.button('View World Bank Data Visualization', 
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/01_World_Bank_Viz.py')
-
-if st.button('View World Map Demo', 
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/02_Map_Demo.py')
+st.write('### Organize your personal/academic tasks!')
