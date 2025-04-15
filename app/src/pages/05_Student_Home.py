88f07823b0b29e7215aa540c980e7c971ba7a475
Era Shah <erashah001@gmail.com>
2025-04-14 20:18:29 -0400

    mcat indents

--- app/src/pages/05_Student_Home.py
+++ app/src/pages/05_Student_Home.py
@@ -15,13 +15,17 @@ st.write('')
 st.write('### Organize your personal/academic life!')
 
 if st.button('GRADES', 
-type='primary', 
-use_container_width=True):
-st.switch_page('pages/05_View_Grades.py')
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/05_View_Grades.py')
 
-if st.button('TASKS', type='primary', use_container_width=True):
-    st.switch_page('pages/22_Student_Tasks.py')
+if st.button('TASKS', 
+             type='primary', 
+             use_container_width=True):
+  st.switch_page('pages/22_Student_Tasks.py')
 
-if st.button('MCAT', type='primary', use_container_width=True):
-    st.switch_page('pages/23_MCAT.py')
+if st.button('MCAT', 
+             type='primary', 
+             use_container_width=True):
+  st.switch_page('pages/23_MCAT.py')
 
