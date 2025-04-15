49495259e7678a1bac493bbd5a64abbc5e6d7c08
srinidhi-datla <datla.sai@northeastern.edu>
2025-04-14 21:07:08 -0400

    fixing parent home page

--- app/src/pages/01_Parent_Home.py
+++ app/src/pages/01_Parent_Home.py
@@ -16,13 +16,13 @@ st.write("Select an action below to manage your tasks, sleep logs, and more.")
 
 # buttons on home page
 if st.button("Grocery List"):
-    st.switch_page("pages/02_Grocery_list.py")  # Replace with your actual file
+    st.switch_page("02_Grocery_list.py")  # Replace with your actual file
 
 if st.button("Tasks"):
-    st.switch_page("pages/03_Task.py")
+    st.switch_page("03_Task.py")
 
 # Sleep log section
 st.markdown("## Sleep Logs")
 
 if st.button("Get Sleep Logs"):
-    st.switch_page("pages/04_Sleep_Logs.py")
+    st.switch_page("04_Sleep_Logs.py")
